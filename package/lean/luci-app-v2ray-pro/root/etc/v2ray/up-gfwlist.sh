3ac48e0dd611fa1d2c5e6882526dd230413b8372
dongjuanyong <38952488+dongjuanyong@users.noreply.github.com>
2019-03-01 12:31:49 +0800

    gfwlist change

--- package/lean/luci-app-v2ray-pro/root/etc/v2ray/up-gfwlist.sh
+++ package/lean/luci-app-v2ray-pro/root/etc/v2ray/up-gfwlist.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
  
-/etc/v2ray/gen-gfwlist.sh > /tmp/ol-gfw.txt
 
-if [ -s "/tmp/ol-gfw.txt" ];then
-	sort -u /etc/v2ray/base-gfwlist.txt /tmp/ol-gfw.txt > /tmp/china-banned
+if [ -s "/tmp/ol_banned.txt" ];then
+	sort -u /etc/v2ray/base-gfwlist.txt /tmp/ol_banned.txt > /tmp/china-banned
 	if ( ! cmp -s /tmp/china-banned /etc/gfwlist/china-banned );then
 		if [ -s "/tmp/china-banned" ];then
 			mv /tmp/china-banned /etc/gfwlist/china-banned
@@ -14,7 +13,18 @@ if [ -s "/tmp/ol-gfw.txt" ];then
 	fi
 fi
 
+if [ -s "/tmp/ol_whitelist.txt" ];then
+	if ( ! cmp -s /tmp/ol_whitelist.txt /etc/gfwlist/whitelist );then
+		mv /tmp/ol_whitelist.txt /etc/gfwlist/whitelist
+		echo "Update GFW-List Done!"
+	else
+		echo "GFW-List No Change!"
+	fi
+fi
+
 rm -f /tmp/gfwlist.txt
-rm -f /tmp/ol-gfw.txt
+rm -f /tmp/ol_banned.txt
+rm -f /tmp/ol_whitelist.txt
+rm -f /tmp/china-banned
 
 /etc/init.d/v2raypro restart
