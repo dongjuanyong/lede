039459b3bd54500781e3f2e6ce8749577bb74ad6
dongjuanyong <38952488+dongjuanyong@users.noreply.github.com>
2020-03-30 16:04:41 +0800

    Revert "luci-app-ssr-plus:Fix update.lua log output and file checking (#4085)"
    This reverts commit 1188a9ffc346b2f01f2641435fe600cda26d2199.

--- package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
+++ package/lean/luci-app-ssr-plus/root/usr/share/shadowsocksr/update.lua
@@ -15,21 +15,17 @@ local log = function(...)
 end
 
 log('正在更新【GFW列表】数据库')
-refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'gfwlist_url', 'https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt') .. " > /tmp/gfw.b64"
+refresh_cmd = "wget-ssl --no-check-certificate -O - " .. uci:get_first('shadowsocksr', 'global', 'gfwlist_url', 'https://cdn.jsdelivr.net/gh/gfwlist/gfwlist/gfwlist.txt') .. " > /tmp/gfw.b64"
 sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 if sret == 0 then
 	luci.sys.call("/usr/bin/ssr-gfw")
 	icount = luci.sys.exec("cat /tmp/gfwnew.txt | wc -l")
 	if tonumber(icount) > 1000 then
-		if nixio.fs.access("/etc/dnsmasq.ssr/gfw_list.conf") then
-			oldcount = luci.sys.exec("cat /etc/dnsmasq.ssr/gfw_list.conf | wc -l")
-		else
-			oldcount = "0"
-		end
+		oldcount = luci.sys.exec("cat /etc/dnsmasq.ssr/gfw_list.conf | wc -l")
 		if tonumber(icount) ~= tonumber(oldcount) then
 			luci.sys.exec("cp -f /tmp/gfwnew.txt /etc/dnsmasq.ssr/gfw_list.conf")
 			luci.sys.exec("cp -f /tmp/gfwnew.txt /tmp/dnsmasq.ssr/gfw_list.conf")
-			log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)/2))
+			log('更新成功！ 新的总纪录数：'.. tostring(tonumber(icount)/2))
 		else
 			log('你已经是最新数据，无需更新！')
 		end
@@ -42,20 +38,16 @@ else
 end
 
 log('正在更新【国内IP段】数据库')
-refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'chnroute_url','https://ispip.clang.cn/all_cn.txt') .. " > /tmp/china_ssr.txt"
+refresh_cmd = "wget-ssl --no-check-certificate -O - ".. uci:get_first('shadowsocksr', 'global', 'chnroute_url','https://ispip.clang.cn/all_cn.txt') .." > /tmp/china_ssr.txt"
 sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 icount = luci.sys.exec("cat /tmp/china_ssr.txt | wc -l")
 if sret == 0 then
 	icount = luci.sys.exec("cat /tmp/china_ssr.txt | wc -l")
 	if tonumber(icount) > 1000 then
-		if nixio.fs.access("/etc/china_ssr.txt") then
-			oldcount = luci.sys.exec("cat /etc/china_ssr.txt | wc -l")
-		else
-			oldcount = "0"
-		end
+		oldcount = luci.sys.exec("cat /etc/china_ssr.txt | wc -l")
 		if tonumber(icount) ~= tonumber(oldcount) then
 			luci.sys.exec("cp -f /tmp/china_ssr.txt /etc/china_ssr.txt")
-			log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)))
+			log('更新成功！ 新的总纪录数：'.. icount)
 		else
 			log('你已经是最新数据，无需更新！')
 		end
@@ -69,7 +61,7 @@ end
 
 if uci:get_first('shadowsocksr', 'global', 'adblock','0') == "1" then
 	log('正在更新【广告屏蔽】数据库')
-	refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'adblock_url','https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt') .. " > /tmp/adnew.conf"
+	refresh_cmd = "wget-ssl --no-check-certificate -O - ".. uci:get_first('shadowsocksr', 'global', 'adblock_url','https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt') .." > /tmp/adnew.conf"
 	sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 	if sret == 0 then
 		luci.sys.call("/usr/bin/ssr-ad")
@@ -83,7 +75,7 @@ if uci:get_first('shadowsocksr', 'global', 'adblock','0') == "1" then
 			if tonumber(icount) ~= tonumber(oldcount) then
 				luci.sys.exec("cp -f /tmp/ad.conf /etc/dnsmasq.ssr/ad.conf")
 				luci.sys.exec("cp -f /tmp/ad.conf /tmp/dnsmasq.ssr/ad.conf")
-				log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)))
+				log('更新成功！ 新的总纪录数：'.. icount)
 			else
 				log('你已经是最新数据，无需更新！')
 			end
@@ -98,20 +90,16 @@ end
 
 --[[
 log('正在更新【Netflix IP段】数据库')
-refresh_cmd = "wget-ssl --no-check-certificate -O- " .. uci:get_first('shadowsocksr', 'global', 'nfip_url','https://raw.githubusercontent.com/QiuSimons/Netflix_IP/master/NF_only.txt') .. " > /tmp/netflixip.list"
+refresh_cmd = "wget-ssl --no-check-certificate -O - ".. uci:get_first('shadowsocksr', 'global', 'nfip_url','https://raw.githubusercontent.com/QiuSimons/Netflix_IP/master/NF_only.txt') .." > /tmp/netflixip.list"
 sret = luci.sys.call(refresh_cmd .. " 2>/dev/null")
 if sret == 0 then
 	luci.sys.call("/usr/bin/ssr-gfw")
 	icount = luci.sys.exec("cat /tmp/netflixip.list | wc -l")
 	if tonumber(icount) > 5 then
-		if nixio.fs.access("/etc/config/netflixip.list") then
-			oldcount = luci.sys.exec("cat /etc/config/netflixip.list | wc -l")
-		else
-			oldcount = "0"
-		end
+		oldcount = luci.sys.exec("cat /etc/config/netflixip.list | wc -l")
 		if tonumber(icount) ~= tonumber(oldcount) then
 			luci.sys.exec("cp -f /tmp/netflixip.list /etc/config/netflixip.list")
-			log('更新成功！ 新的总纪录数：' .. tostring(tonumber(icount)))
+			log('更新成功！ 新的总纪录数：'.. icount)
 		else
 			log('你已经是最新数据，无需更新！')
 		end
@@ -123,3 +111,5 @@ else
 	log('更新失败！')
 end
 --]]
+
+luci.sys.call("/etc/init.d/dnsmasq reload")
